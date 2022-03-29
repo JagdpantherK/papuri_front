39f4a25244523b868c471513c686491537a9c8b8
kadyr <jagpanthertitanium@gmail.com>
2022-03-29 17:29:56 +0600

    Added eng locale messages

--- src/locales/en/index.js
+++ src/locales/en/index.js
@@ -2,5 +2,7 @@ export default {
     test: "english",
     login: "Sign In",
     signUp: "Sign Up",
+    save: "Save",
+    recentlyAdded: "Recently Added",
     fileNotChosen: "file not chosen"
 }
